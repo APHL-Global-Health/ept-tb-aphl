4273326e10db97b9cb269cb7ad2629d3eb7605e6
Thomas Mapesa <mapesa@gmail.com>
2020-06-23 15:15:53 +0300

    Remove redefining constant warning

--- public/index.php
+++ public/index.php
@@ -13,9 +13,6 @@ defined('APPLICATION_ENV')
 defined('UPLOAD_PATH')
     || define('UPLOAD_PATH', realpath(dirname(__FILE__) . '/uploads'));
 
-defined('K_PATH_CACHE')
-    || define('K_PATH_CACHE', realpath(dirname(__FILE__) . '/uploads/tmp'));
-
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
